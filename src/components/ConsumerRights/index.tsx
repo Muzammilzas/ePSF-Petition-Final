1e740f365ceb3a446de83bfac719c81d2dab54c5
Muzammilzas <muzammil.arfat@st.oth-regensburg.de>
2025-03-22 00:25:34 +0500

    removed header and footer.

--- src/components/ConsumerRights/index.tsx
+++ src/components/ConsumerRights/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import Navbar from './sections/Navbar';
 import Header from './sections/Header';
 import Problem from './sections/Problem';
 import DebtInheritance from './sections/DebtInheritance';
 import Guide from './sections/Guide';
 import Plan from './sections/Plan';
 import DarkCallToAction from './sections/DarkCallToAction';
-import Footer from './sections/Footer';
 import Success from './sections/Success';
 import Failure from './sections/Failure';
 import { colors } from './styles';
@@ -15,7 +13,6 @@ import { colors } from './styles';
 const ConsumerRights: React.FC = () => {
   return (
     <Box>
-      <Navbar />
       <Box sx={{ bgcolor: colors.background.light }}>
         <Header />
         <Problem />
@@ -23,7 +20,6 @@ const ConsumerRights: React.FC = () => {
         <Guide />
         <Plan />
         <DarkCallToAction />
-        <Footer />
         {/* Success and Failure components are rendered conditionally based on form submission */}
       </Box>
     </Box>
